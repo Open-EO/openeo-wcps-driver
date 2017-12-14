c225928e8708082694c727946125a7c88b410ec2
Alexander Jacob <alexander.jacob@eurac.edu>
2017-12-14 14:55:54 +0100

    fix error in filter range setting

--- src/main/java/eu/openeo/backend/wcps/WCPSQueryFactory.java
+++ src/main/java/eu/openeo/backend/wcps/WCPSQueryFactory.java
@@ -11,85 +11,88 @@ import eu.openeo.backend.wcps.domain.Collection;
 import eu.openeo.backend.wcps.domain.Filter;
 
 public class WCPSQueryFactory {
-	
+
 	private StringBuilder wcpsStringBuilder;
 	private Vector<Collection> collectionIDs;
 	private Vector<Filter> filters;
 	private Vector<Aggregate> aggregates;
-	
+
 	Logger log = Logger.getLogger(this.getClass());
-	
+
 	/**
 	 * Creates WCPS query from openEO process Graph
+	 * 
 	 * @param openEOGraph
 	 */
 	public WCPSQueryFactory(JSONObject openEOGraph) {
 		collectionIDs = new Vector<Collection>();
-		aggregates =  new Vector<Aggregate>();
+		aggregates = new Vector<Aggregate>();
 		filters = new Vector<Filter>();
 		wcpsStringBuilder = new StringBuilder("for ");
-		if(openEOGraph.containsKey(new String("process_graph"))) {
+		if (openEOGraph.containsKey(new String("process_graph"))) {
 			parseOpenEOProcessGraph((JSONObject) openEOGraph.get(new String("process_graph")));
-			
+
 		}
-		for (int c = 1 ; c <= collectionIDs.size(); c++){
+		for (int c = 1; c <= collectionIDs.size(); c++) {
 			wcpsStringBuilder.append("$c" + c);
-			if(c > 1) {
+			if (c > 1) {
 				wcpsStringBuilder.append(", ");
 			}
 		}
 		wcpsStringBuilder.append(" in ( ");
-		for (int c = 1 ; c <= collectionIDs.size(); c++){
-			wcpsStringBuilder.append(collectionIDs.get(c-1).getName() + " ");
+		for (int c = 1; c <= collectionIDs.size(); c++) {
+			wcpsStringBuilder.append(collectionIDs.get(c - 1).getName() + " ");
 		}
 		wcpsStringBuilder.append(") return encode ( ");
-		for(int a = 0; a < aggregates.size(); a++) {
-			if(aggregates.get(a).getOperator().equals("NDVI")) {
+		for (int a = 0; a < aggregates.size(); a++) {
+			if (aggregates.get(a).getOperator().equals("NDVI")) {
 				wcpsStringBuilder.append(createNDVIWCPSString("$c1", aggregates.get(a)));
 			}
 		}
-		if(filters.size() > 0) {
+		if (filters.size() > 0) {
 			wcpsStringBuilder.append(createFilteredCollectionString("$c1"));
 		}
 		wcpsStringBuilder.append(", netcdf )");
 	}
-	
+
 	/**
-	 * Helper Method to create a string describing an arbitrary filtering as defined from the process graph
+	 * Helper Method to create a string describing an arbitrary filtering as defined
+	 * from the process graph
+	 * 
 	 * @param collectionName
 	 * @return
 	 */
 	private String createFilteredCollectionString(String collectionName) {
 		StringBuilder stringBuilder = new StringBuilder(collectionName);
 		stringBuilder.append("[");
-		for(int f = 0; f < filters.size(); f++) {
+		for (int f = 0; f < filters.size(); f++) {
 			Filter filter = filters.get(f);
 			String axis = filter.getAxis();
 			stringBuilder.append(axis + "(");
-			if(axis.contains("DATE")) {
-				stringBuilder.append("\""); 
+			if (axis.contains("DATE")) {
+				stringBuilder.append("\"");
 			}
 			stringBuilder.append(filter.getLowerBound());
-			if(axis.contains("DATE")) {
-				stringBuilder.append("\""); 
+			if (axis.contains("DATE")) {
+				stringBuilder.append("\"");
 			}
 			stringBuilder.append(":");
-			 if(axis.contains("DATE")) {
-					stringBuilder.append("\""); 
-				}
-			 stringBuilder.append(filter.getLowerBound());
-			 if(axis.contains("DATE")) {
-					stringBuilder.append("\""); 
-				}
-			 stringBuilder.append(")");
-			if(f < filters.size() - 1) {
+			if (axis.contains("DATE")) {
+				stringBuilder.append("\"");
+			}
+			stringBuilder.append(filter.getUpperBound());
+			if (axis.contains("DATE")) {
+				stringBuilder.append("\"");
+			}
+			stringBuilder.append(")");
+			if (f < filters.size() - 1) {
 				stringBuilder.append(",");
 			}
 		}
 		stringBuilder.append("]");
 		return stringBuilder.toString();
 	}
-	
+
 	private String createNDVIWCPSString(String collectionName, Aggregate ndviAggregate) {
 		String redBandName = ndviAggregate.getParams().get(0);
 		String nirBandName = ndviAggregate.getParams().get(1);
@@ -105,7 +108,7 @@ public class WCPSQueryFactory {
 		filters.removeAllElements();
 		return stringBuilder.toString();
 	}
-	
+
 	private String createBandSubsetString(String collectionName, String bandName, String subsetString) {
 		StringBuilder stringBuilder = new StringBuilder(collectionName);
 		stringBuilder.append(".");
@@ -113,15 +116,16 @@ public class WCPSQueryFactory {
 		stringBuilder.append(subsetString);
 		return stringBuilder.toString();
 	}
-	
+
 	/**
 	 * returns constructed query as String object
+	 * 
 	 * @return String WCPS query
 	 */
-	public String getWCPSString(){
+	public String getWCPSString() {
 		return wcpsStringBuilder.toString();
 	}
-	
+
 	/**
 	 * 
 	 * @param processParent
@@ -129,40 +133,38 @@ public class WCPSQueryFactory {
 	 */
 	private JSONObject parseOpenEOProcessGraph(JSONObject processParent) {
 		JSONObject result = null;
-		for (Object key : processParent.keySet()) {			
-	        String keyStr = (String)key;
-	        if(keyStr.equals("process_id")) {
-	        	String name = (String) processParent.get(keyStr);
-	        	log.debug("currently working on: " + name);
-	        	if(name.contains("filter")) {
-	        		createFilterFromProcess(processParent);
-	        	}else {
-	        		createAggregateFromProcess(processParent);
-	        	}
-	        } else
-	        if(keyStr.equals("args")) {
-	        	JSONObject argsObject = (JSONObject) processParent.get(keyStr);
-		        for (Object argsKey : argsObject.keySet()) {
-		        	String argsKeyStr = (String)argsKey;
-		        	if(argsKeyStr.equals("collections")) {
-		        		JSONArray collections = (JSONArray) argsObject.get(argsKey);
-		        		result = parseOpenEOProcessGraph((JSONObject) collections.get(0));
-		        		if(result == null) {
-		        			result = (JSONObject) collections.get(0);
-		        			break;
-		        		}
-		        	}
-		        }
-	        } else 
-	        if(keyStr.equals("collection_id")) {
-	        	String name = (String) processParent.get(keyStr);
-	        	collectionIDs.add(new Collection(name));
-	        	log.debug("found actual dataset: " + name);
-	        }
-	    }
+		for (Object key : processParent.keySet()) {
+			String keyStr = (String) key;
+			if (keyStr.equals("process_id")) {
+				String name = (String) processParent.get(keyStr);
+				log.debug("currently working on: " + name);
+				if (name.contains("filter")) {
+					createFilterFromProcess(processParent);
+				} else {
+					createAggregateFromProcess(processParent);
+				}
+			} else if (keyStr.equals("args")) {
+				JSONObject argsObject = (JSONObject) processParent.get(keyStr);
+				for (Object argsKey : argsObject.keySet()) {
+					String argsKeyStr = (String) argsKey;
+					if (argsKeyStr.equals("collections")) {
+						JSONArray collections = (JSONArray) argsObject.get(argsKey);
+						result = parseOpenEOProcessGraph((JSONObject) collections.get(0));
+						if (result == null) {
+							result = (JSONObject) collections.get(0);
+							break;
+						}
+					}
+				}
+			} else if (keyStr.equals("collection_id")) {
+				String name = (String) processParent.get(keyStr);
+				collectionIDs.add(new Collection(name));
+				log.debug("found actual dataset: " + name);
+			}
+		}
 		return result;
 	}
-	
+
 	/**
 	 * 
 	 * @param process
@@ -171,73 +173,69 @@ public class WCPSQueryFactory {
 		boolean isTemporalFilter = false;
 		boolean isBoundBoxFilter = false;
 		for (Object key : process.keySet()) {
-			String keyStr = (String)key;
-			if(keyStr.equals("process_id")) {
-	        	String name = (String) process.get(keyStr);
-	        	log.debug("currently working on: " + name);
-	        	if(name.contains("date")) {
-        			isTemporalFilter = true;
-        		} else
-        		if(name.contains("bbox")) {
-        			isBoundBoxFilter = true;
-        		}
-	        }
+			String keyStr = (String) key;
+			if (keyStr.equals("process_id")) {
+				String name = (String) process.get(keyStr);
+				log.debug("currently working on: " + name);
+				if (name.contains("date")) {
+					isTemporalFilter = true;
+				} else if (name.contains("bbox")) {
+					isBoundBoxFilter = true;
+				}
+			}
 		}
 		for (Object key : process.keySet()) {
-			String keyStr = (String)key;
-			if(keyStr.equals("args")) {
-	        	JSONObject argsObject = (JSONObject) process.get(keyStr);
-	        	if(isTemporalFilter) {
-	        		createDateRangeFilterFromArgs(argsObject);
-	        	}
-	        	if(isBoundBoxFilter) {
-	        		createBoundingBoxFilterFromArgs(argsObject);
-	        	}
-		        
-	        }
+			String keyStr = (String) key;
+			if (keyStr.equals("args")) {
+				JSONObject argsObject = (JSONObject) process.get(keyStr);
+				if (isTemporalFilter) {
+					createDateRangeFilterFromArgs(argsObject);
+				}
+				if (isBoundBoxFilter) {
+					createBoundingBoxFilterFromArgs(argsObject);
+				}
+
+			}
 		}
 	}
-	
+
 	private void createDateRangeFilterFromArgs(JSONObject argsObject) {
 		String fromDate = null;
 		String toDate = null;
 		for (Object argsKey : argsObject.keySet()) {
-        	String argsKeyStr = (String)argsKey;
-        	if(argsKeyStr.equals("from")) {
-        		fromDate = (String) argsObject.get(argsKey);
-        	} else
-    		if(argsKeyStr.equals("to")) {
-        		toDate = (String) argsObject.get(argsKey);
-        	}
-        }
-		if(fromDate != null && toDate != null)
-		this.filters.add(new Filter("DATE", fromDate, toDate));
+			String argsKeyStr = (String) argsKey;
+			if (argsKeyStr.equals("from")) {
+				fromDate = (String) argsObject.get(argsKey);
+			} else if (argsKeyStr.equals("to")) {
+				toDate = (String) argsObject.get(argsKey);
+			}
+		}
+		if (fromDate != null && toDate != null)
+			this.filters.add(new Filter("DATE", fromDate, toDate));
 	}
-	
+
 	private void createBoundingBoxFilterFromArgs(JSONObject argsObject) {
 		String left = null;
 		String right = null;
 		String top = null;
 		String bottom = null;
 		for (Object argsKey : argsObject.keySet()) {
-        	String argsKeyStr = (String)argsKey;
-        	if(argsKeyStr.equals("left")) {
-        		left = "" + argsObject.get(argsKey).toString();
-        	} else
-    		if(argsKeyStr.equals("right")) {
-    			right = "" + argsObject.get(argsKey).toString();
-        	}
-        	if(argsKeyStr.equals("top")) {
-        		top = "" + argsObject.get(argsKey).toString();
-        	} else
-    		if(argsKeyStr.equals("bottom")) {
-    			bottom = "" + argsObject.get(argsKey).toString();
-        	}
-        }
+			String argsKeyStr = (String) argsKey;
+			if (argsKeyStr.equals("left")) {
+				left = "" + argsObject.get(argsKey).toString();
+			} else if (argsKeyStr.equals("right")) {
+				right = "" + argsObject.get(argsKey).toString();
+			}
+			if (argsKeyStr.equals("top")) {
+				top = "" + argsObject.get(argsKey).toString();
+			} else if (argsKeyStr.equals("bottom")) {
+				bottom = "" + argsObject.get(argsKey).toString();
+			}
+		}
 		this.filters.add(new Filter("E", left, right));
 		this.filters.add(new Filter("N", top, bottom));
 	}
-	
+
 	/**
 	 * 
 	 * @param process
@@ -246,50 +244,48 @@ public class WCPSQueryFactory {
 		boolean isTemporalAggregate = false;
 		boolean isNDVIAggregate = false;
 		for (Object key : process.keySet()) {
-			String keyStr = (String)key;
-			if(keyStr.equals("process_id")) {
-	        	String name = (String) process.get(keyStr);
-	        	log.debug("currently working on: " + name);
-	        	if(name.contains("date") || name.contains("time")) {
-	        		isTemporalAggregate = true;
-        		} else
-        		if(name.contains("NDVI")) {
-        			isNDVIAggregate = true;
-        		}
-	        }
+			String keyStr = (String) key;
+			if (keyStr.equals("process_id")) {
+				String name = (String) process.get(keyStr);
+				log.debug("currently working on: " + name);
+				if (name.contains("date") || name.contains("time")) {
+					isTemporalAggregate = true;
+				} else if (name.contains("NDVI")) {
+					isNDVIAggregate = true;
+				}
+			}
 		}
 		for (Object key : process.keySet()) {
-			String keyStr = (String)key;
-			if(keyStr.equals("args")) {
-	        	JSONObject argsObject = (JSONObject) process.get(keyStr);
-	        	if(isTemporalAggregate) {
-	        		
-	        	}
-	        	if(isNDVIAggregate) {
-	        		createNDVIAggregateFromProcess(argsObject);
-	        	}
-		        
-	        }
+			String keyStr = (String) key;
+			if (keyStr.equals("args")) {
+				JSONObject argsObject = (JSONObject) process.get(keyStr);
+				if (isTemporalAggregate) {
+
+				}
+				if (isNDVIAggregate) {
+					createNDVIAggregateFromProcess(argsObject);
+				}
+
+			}
 		}
 	}
-	
+
 	private void createNDVIAggregateFromProcess(JSONObject argsObject) {
 		String red = null;
 		String nir = null;
 		for (Object argsKey : argsObject.keySet()) {
-        	String argsKeyStr = (String)argsKey;
-        	if(argsKeyStr.equals("red")) {
-        		red = "" + argsObject.get(argsKey).toString();
-        	} else
-    		if(argsKeyStr.equals("nir")) {
-    			nir = "" + argsObject.get(argsKey).toString();
-        	}
-        }
+			String argsKeyStr = (String) argsKey;
+			if (argsKeyStr.equals("red")) {
+				red = "" + argsObject.get(argsKey).toString();
+			} else if (argsKeyStr.equals("nir")) {
+				nir = "" + argsObject.get(argsKey).toString();
+			}
+		}
 		Vector<String> params = new Vector<String>();
 		params.add(red);
 		params.add(nir);
-		if(red != null && nir != null)
-		aggregates.add(new Aggregate(new String("feature"), new String("NDVI"), params));
+		if (red != null && nir != null)
+			aggregates.add(new Aggregate(new String("feature"), new String("NDVI"), params));
 	}
 
 }
