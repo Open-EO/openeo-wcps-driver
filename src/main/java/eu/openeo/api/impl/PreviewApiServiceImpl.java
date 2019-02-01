9aa8474c116c7b39e245f33ad34193f24ca93406
Alexander Jacob <alexander.jacob@eurac.edu>
2019-02-01 14:50:31 +0100

    updated error in get_collection process name. Added new custom headers to job sumission response and options

--- src/main/java/eu/openeo/api/impl/PreviewApiServiceImpl.java
+++ src/main/java/eu/openeo/api/impl/PreviewApiServiceImpl.java
@@ -42,7 +42,7 @@ public class PreviewApiServiceImpl extends PreviewApiService {
 
 	@Override
 	public Response previewOptions(SecurityContext securityContext) throws NotFoundException {
-		return Response.ok().build();
+		return Response.ok().header("Access-Control-Expose-Headers", "OpenEO-Identifier, OpenEO-Costs").build();
 	}
 
 	@Override
@@ -72,7 +72,7 @@ public class PreviewApiServiceImpl extends PreviewApiService {
 			job.setStatus(JobStatus.FINISHED);
 			job.setUpdated(new Date().toGMTString());
 //			jobDao.update(job);
-			return Response.ok(response, ConvenienceHelper.getMimeTypeFromOutput(outputFormat)).build();
+			return Response.ok(response, ConvenienceHelper.getMimeTypeFromOutput(outputFormat)).header("Access-Control-Expose-Headers", "OpenEO-Identifier, OpenEO-Costs").build();
 		} catch (MalformedURLException e) {
 			log.error("An error occured when creating URL from job preview query: " + e.getMessage());
 			return Response.serverError().entity("An error occured when creating URL from job query: " + e.getMessage())
