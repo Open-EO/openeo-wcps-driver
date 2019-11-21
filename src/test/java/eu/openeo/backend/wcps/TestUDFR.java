d6d82ca45c2b5bc3b62d3495447f9737591ae89b
Alexander Jacob <alexander.jacob@eurac.edu>
2019-11-21 16:28:15 +0100

    code cleanup

--- src/test/java/eu/openeo/backend/wcps/TestUDFR.java
+++ src/test/java/eu/openeo/backend/wcps/TestUDFR.java
@@ -7,11 +7,9 @@ import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.net.HttpURLConnection;
 import java.net.URL;
-import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 
-import org.json.JSONObject;
 import org.junit.jupiter.api.Test;
 
 public class TestUDFR {
